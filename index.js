c53d9eb1c4a496244e75db0f7f3660c8c22f4c49
JP <john.pardy2@gmail.com>
2023-03-05 20:09:23 +0000

    updates

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
-
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const render = require("./src/page-template.js");
+
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./src/page-template.js");
-
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
+// Write Code to gather information about the development team members, and render the HTML file.
 function createEngineer(team) {
   inquirer
     .prompt([
-      // Engineer name
-      // Engineer id
-      // Engineer email
-      // Engineer GitHub username
       {
         type: "input",
         name: "name",
@@ -121,6 +115,7 @@ function createTeam(team) {
         // write html to a file index.html using fs library
         fs.writeFile(outputPath, html, (err) => {
           if (err) {
+            console.log(err);
             console.log("Failed to write HTML file");
           }
         });
